0b29af12f00cdc806bc4d6d195eaec4843c1f774
Martin Hundebøll <martin@hundeboll.net>
2012-06-06 00:08:47 +0200

    Add license

--- block.py
+++ block.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <mhu@es.aau.dk> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return.
+#
+# This code is proof-of-concept and is meant for demonstration purposes only.
+# I apologize for any eye-damage caused by the low quality of the code below.
+#
+# - Martin Hundebøll
+# ----------------------------------------------------------------------------
 
 import sys
 import os
